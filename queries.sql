323cb2c4eef05e274d4c29f9d8310edfbb58e231
elinaulm <120288584+elinaulm@users.noreply.github.com>
2024-05-03 14:13:17 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -6,31 +6,29 @@ from customers;
 
 /*шаг 5*/
 select
-    CONCAT(first_name, ' ', last_name) as seller,
-    /*селектим имя продавца, подсчитываем количество операций по айдишникам продавцов, считаем суммарную выручку*/
-    COUNT(sales_person_id) as operations,
+    CONCAT(e.first_name, ' ', e.last_name) as seller,
+    COUNT(s.sales_person_id) as operations,
     FLOOR(SUM(p.price * s.quantity)) as income
 from sales as s
 inner join employees as e
     on s.sales_person_id = e.employee_id
 inner join products as p
     on s.product_id = p.product_id
-group by CONCAT(first_name, ' ', last_name)
+group by CONCAT(e.first_name, ' ', e.last_name)
 order by income desc
 limit 10;
 
 
 with tab as (
     select
-        /*делаем соединение, находим среднюю выручку каждого продавца и через подзапрос находим среднее по всем продавцам*/
-        CONCAT(first_name, ' ', last_name) as seller,
+        CONCAT(e.first_name, ' ', e.last_name) as seller,
         FLOOR(AVG(p.price * s.quantity)) as average_income
     from sales as s
     inner join employees as e
         on s.sales_person_id = e.employee_id
     inner join products as p
         on s.product_id = p.product_id
-    group by CONCAT(first_name, ' ', last_name)
+    group by CONCAT(e.first_name, ' ', e.last_name)
 )
 
 select
@@ -43,9 +41,9 @@ order by average_income;
 
 
 select
-    CONCAT(first_name, ' ', last_name) as seller,
+    CONCAT(e.first_name, ' ', e.last_name) as seller,
     /*селектим имя продавца, день недели и суммарную выручку по дням недели*/
-    TO_CHAR(sale_date, 'day') as day_of_week,
+    TO_CHAR(s.sale_date, 'day') as day_of_week,
     FLOOR(SUM(p.price * s.quantity)) as income
 from sales as s
 inner join employees as e
@@ -53,10 +51,10 @@ inner join employees as e
 inner join products as p
     on s.product_id = p.product_id
 group by
-    CONCAT(first_name, ' ', last_name),
+    CONCAT(e.first_name, ' ', e.last_name),
     day_of_week,
-    EXTRACT(isodow from sale_date)
-order by EXTRACT(isodow from sale_date), seller;
+    EXTRACT(isodow from s.sale_date)
+order by EXTRACT(isodow from s.sale_date), seller;
 
 
 /*шаг 6*/
@@ -85,36 +83,38 @@ group by 1
 order by 1;
 
 
-select
-    t.customer,
-    t.sale_date,
-    --соединяем имя и фамилию сотрудника, выделяем самого первого
-    min(concat(e.first_name, ' ', e.last_name)) as seller
-from (
-    select                                   --подзапрос№2
+with tab as (
+    select
         id,
         customer,
-        min(date) as sale_date   --вытаскиваем самую раннюю дату
+        min(sale_date) as sale_date
     from (
-        select                                    --подзапрос№1
+        select
             p.price,
             --скрещиваем имя и фамилию покупателя
             c.customer_id as id,
-            s.sale_date as date,
+            s.sale_date,
             concat(c.first_name, ' ', c.last_name) as customer
         from sales as s
         inner join products as p
-            on p.product_id = s.product_id --соединяем таблицы по id
+            on s.product_id = p.product_id --соединяем таблицы по id
         inner join customers as c
-            on c.customer_id = s.customer_id --соединяем таблицы по id
+            on s.customer_id = c.customer_id --соединяем таблицы по id
     ) as fool
     where price = 0 --выделяем только строки со значением цены 0
     group by 1, 2 --группируем по id и customer
-) as t
+)
+
+select
+    tab.customer,
+    tab.sale_date,
+    --соединяем имя и фамилию сотрудника, выделяем самого первого
+    min(concat(e.first_name, ' ', e.last_name)) as seller
+from tab
 inner join sales as s
-    on t.id = s.customer_id --соединяем таблицы по id
+    on tab.id = s.customer_id --соединяем таблицы по id
 inner join employees as e
     on s.sales_person_id = e.employee_id --соединяем по id
-group by 1, 2, t.id --группируем по customer, sale_date и id
--- получаем таблицу с клиентами что совершили свою первую покупку по акции
-order by t.id; --сортируем по id в порядке возрастания
+group by tab.customer, tab.sale_date;
+
+
